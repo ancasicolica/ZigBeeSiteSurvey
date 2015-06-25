74915ed74390a1c885ac937a3f5996a98f89a4d1
Christian Kuster <christian.kuster@kaba.com>
2015-06-25 21:32:45 +0200

    Progress bars added

--- public/js/surveyctrl.js
+++ public/js/surveyctrl.js
@@ -8,6 +8,9 @@ surveyControl.controller('surveyCtrl', ['$scope', '$http', function ($scope, $ht
   $scope.panel = 'networks';
   $scope.networks = [];
   $scope.networkScanActive = false;
+  $scope.measurements = [];
+  $scope.rssiMin = -100;
+  $scope.rssiMax = 0;
 
   $(document).ready(function () {
     $scope.getNetworks();
@@ -20,8 +23,34 @@ surveyControl.controller('surveyCtrl', ['$scope', '$http', function ($scope, $ht
   $scope.survey = function(network) {
     $scope.currentNetwork = network;
     $scope.panel = 'survey';
+    $scope.measurements = [];
+    $scope.updateCurrentNetworkData();
   };
 
+  /**
+   * Returns the last entry of the measurement
+   * @returns {*}
+   */
+  $scope.getLatestMeasurementEntry = function() {
+    if($scope.measurements.length === 0) {
+      return {rssi:0, lqi:0};
+    }
+    return _.last($scope.measurements);
+  };
+
+  /**
+   * Return the class for the progressbar associated with the given rssi value
+   * @param rssi
+   */
+  $scope.getRssiClass = function(rssi) {
+    if (rssi < -63) {
+      return 'progress-bar-danger';
+    }
+    if (rssi > -50) {
+      return 'progress-bar-success'
+    }
+    return 'progress-bar-warning';
+  };
   /**
    * Get information about all networks
    */
@@ -36,6 +65,7 @@ surveyControl.controller('surveyCtrl', ['$scope', '$http', function ($scope, $ht
           $scope.networks = [];
         }
         $scope.networkScanActive = false;
+
       }).
       error(function (data, status) {
         console.log('error:');
@@ -46,6 +76,35 @@ surveyControl.controller('surveyCtrl', ['$scope', '$http', function ($scope, $ht
       });
   };
 
+  $scope.calculateRssiPercent = function(rssi) {
+    return 100 - Math.abs(rssi / ($scope.rssiMax - $scope.rssiMin)) * 100;
+  };
+  /**
+   * Get information about all networks
+   */
+  $scope.updateCurrentNetworkData = function() {
+    $scope.networkScanActive = true;
+    $http.get('/scan/' + $scope.currentNetwork.channel + '/' + $scope.currentNetwork.panId).
+      success(function (data) {
+        if (data.status === 'ok') {
+          if (data.networks.length > 0) {
+            var m = data.networks[0];
+            m.rssiPercent = $scope.calculateRssiPercent(m.rssi);
+            $scope.measurements.push(m);
+          }
+        }
+        $scope.networkScanActive = false;
+
+        // start again (don't forget to stop this one time...)
+        _.delay($scope.updateCurrentNetworkData, 500);
+      }).
+      error(function (data, status) {
+        console.log('error:');
+        console.log(data);
+        console.log(status);
+        $scope.networkScanActive = false;
+      });
+  };
 
 
 }]);
--- routes/scan.js
+++ routes/scan.js
@@ -13,22 +13,30 @@ var networkScanRequest = require('../lib/tasks/networkScanRequest');
  * Get the ranking list
  */
 router.get('/all', function (req, res) {
-  console.log('/all');
-  try {
-    networkScanRequest.start({}, function (err, networks) {
-      console.log('   callback');
-      if (err) {
-        res.send({status: 'error', message: err.message});
-      }
-      else {
-        res.send({status: 'ok', networks: networks});
-      }
+  networkScanRequest.start({}, function (err, networks) {
+    if (err) {
+      res.send({status: 'error', message: err.message});
+    }
+    else {
+      res.send({status: 'ok', networks: networks});
+    }
+  });
 
-    });
-  }
-  catch(e) {
-    console.error(e);
-  }
+});
+
+/**
+ * Get the ranking list
+ */
+router.get('/:channel/:panId', function (req, res) {
+  networkScanRequest.start({channels: [req.params.channel], panId: req.params.panId}, function (err, networks) {
+    if (err) {
+      res.send({status: 'error', message: err.message});
+    }
+    else {
+      res.send({status: 'ok', networks: networks});
+    }
+  });
+
 });
 
 
