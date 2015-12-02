810ca3e4f7dddf8c95e89875ab45456f8c884b05
Christian Kuster <christian@kusti.ch>
2015-12-02 19:27:47 +0100

    CSS works

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
+var scanner = require('../lib/scanner');
 
 /* GET home page. */
 router.get('/', function (req, res) {
   res.render('index', {title: 'ZigBee Site Survey'});
+  scanner.enable();
 });
 router.get('/test', function (req, res) {
   res.render('test', {title: 'ZigBee Site Survey'});
